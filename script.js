40acd01c0d2307452c3abacd691586c4181fddc4
MariIvChin <mari.chin1976@gmail.com>
2022-10-01 22:11:11 +0300

    fix

--- script.js
+++ script.js
@@ -1,107 +1,125 @@
-const todoManager = new Todo();
+window.addEventListener("load", () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-const todoInput = document.getElementById("todo-input");
-const todoBtn = document.getElementById("todo-btn");
-const removeAllBtn = document.getElementById("remove-all-btn");
-const todoList = document.getElementById("todo-list");
-const errorMessage = document.getElementById("error-message");
+  const todoInput = document.querySelector("#todo-input");
 
-todoBtn.addEventListener("click", addTodo);
+  todoInput.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-todoList.addEventListener("click", function (e) {
-  const todoId = Number(e.target.closest("li[data-todo-id]").dataset.todoId);
-  const shouldRemove = e.target.classList.contains("bi-trash");
+    const todo = {
+      content: e.target.elements.content.value,
+      done: false,
+      createdAt: new Date().getTime(),
+    };
 
-  if (shouldRemove) {
-    todoManager.remove(todoId);
-  } else {
-    todoManager.changeDone(todoId);
-  }
-  renderList();
+    if (todo.content.length < 2) {
+      alert("Please write your task in input!");
+      return;
+    }
+
+    todos.push(todo);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    // Reset the form
+    e.target.reset();
+
+    renderTodos();
+  });
+
+  renderTodos();
 });
 
-removeAllBtn.addEventListener("click", function (e) {
-  // const todoId = Number(e.target.closest("li[data-todo-id]").dataset.todoId);
-  const shouldRemove = e.target.classList.contains("bi-trash");
-  if (shouldRemove) {
-    todoManager.removeAll();
-  }
-  return;
-});
+function renderTodos() {
+  const todoList = document.querySelector("#todo-list");
+  todoList.innerHTML = "";
 
-function addTodo() {
-  try {
-    todoManager.add(/* todoManager */ todoInput.value);
-    resetInput();
-    clearErrorMessage();
-    renderList();
-    todoInput.focus();
-  } catch (err) {
-    setErrorMessage(err.message);
-  }
+  todos.forEach((todo) => {
+    const todoItem = document.createElement("li");
+    todoItem.classList.add(
+      "list-group-item",
+      "d-flex",
+      "justify-content-between",
+      "mb-2",
+      "w-75"
+    );
+
+    const input = document.createElement("input");
+    const content = document.createElement("input");
+    const editBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+
+    input.type = "checkbox";
+    input.classList.add("form-check-input", "me-2", "cursor-pointer");
+    input.checked = todo.done;
+    content.classList.add(
+      "form-control",
+      "flex-fill",
+      "text-uppercase",
+      "fw-bold",
+      "col-2",
+      "me-2",
+      "w-50"
+    );
+    content.setAttribute("readonly", "readonly");
+    editBtn.classList.add(
+      "btn",
+      "btn-success",
+      "btn-sm",
+      "cursor-pointer",
+      "me-2"
+    );
+    deleteBtn.classList.add("btn", "btn-danger", "btn-sm", "cursor-pointer");
+
+    content.value = todo.content;
+    editBtn.innerHTML = "Edit";
+    deleteBtn.innerHTML = "Delete";
+
+    todoItem.appendChild(input);
+    todoItem.appendChild(content);
+    todoItem.appendChild(editBtn);
+    todoItem.appendChild(deleteBtn);
+
+    todoList.appendChild(todoItem);
+
+    if (todo.done) {
+      content.classList.add("text-decoration-line-through") &
+        content.setAttribute("disabled", true);
+    }
+
+    input.addEventListener("change", (e) => {
+      todo.done = e.target.checked;
+      localStorage.setItem("todos", JSON.stringify(todos));
+
+      if (todo.done) {
+        todoItem.classList.add("done");
+      } else {
+        todoItem.classList.remove("done");
+      }
+
+      renderTodos();
+    });
+
+    editBtn.addEventListener("click", (e) => {
+      if ((todo.done !== true) & (editBtn.innerText.toLowerCase() == "edit")) {
+        editBtn.innerText = "Save";
+        // const input = content.querySelector("input");
+        content.removeAttribute("readonly");
+        content.focus();
+      }
+      content.addEventListener("blur", (e) => {
+        editBtn.innerHTML = "Edit";
+        content.setAttribute("readonly", "readonly");
+        todo.content = e.target.value;
+        localStorage.setItem("todos", JSON.stringify(todos));
+        renderTodos();
+      });
+    });
+
+    deleteBtn.addEventListener("click", (e) => {
+      todos = todos.filter((t) => t != todo);
+      localStorage.setItem("todos", JSON.stringify(todos));
+      renderTodos();
+    });
+  });
 }
-
-function renderList() {
-  const todos = todoManager.getTodos();
-  // refreshStatus();
-  let html = "";
-  for (const todo of todos) {
-    html += `<li data-todo-id="${
-      todo.id
-    }" class="list-group-item d-flex justify-content-between cursor-pointer">
-    <input
-      class="form-check-input me-2 cursor-pointer"
-      type="checkbox"
-      ${todo.isDone ? "checked" : ""}
-    />
-    <span class="flex-fill text-uppercase fw-bold col-2
-      ${todo.isDone ? "text-decoration-line-through" : ""}">
-      ${todo.text}
-      </span
-      >
-      <span class="flex-fill col-4">14.09.2022 15:30:10</span>
-      <button
-        id="edit-btn"
-        class="btn btn-success btn-sm cursor-pointer"
-      >
-        <i class="bi bi-pencil"></i>
-      </button>
-      <button
-        id="remove-btn"
-        class="btn btn-danger btn-sm cursor-pointer"
-      >
-        <i class="bi bi-trash text-Light"></i>
-      </button>
-      </li>`;
-  }
-  todoList.innerHTML = html;
-}
-
-function resetInput() {
-  todoInput.value = "";
-}
-
-function setErrorMessage(error) {
-  errorMessage.innerHTML = `<span>${error}</span>`;
-}
-
-function clearErrorMessage() {
-  errorMessage.innerHTML = "";
-}
-
-// todoBtn.addEventListener("click", todoManager.add);
-
-// function addEventListener(eventName, functionToRun) {
-//   functionToRun(/* undefine/window */ new Event());
-// }
-
-// function refreshStatus() {
-//   const amountDone = todoManager.getTodos().reduce((completedTodos, todo) => {
-//     console.log(completedTodos, todo);
-//     return todo.isDone ? completedTodos + 1 : completedTodos;
-//   }, 0);
-
-//   todoStatus.innerHTML = `${amountDone}/${todoManager.getTodos().length}`;
-// }
-
-// const amountDone = todoManager.getTodos().filter((todo) => todo.isDone).length;
